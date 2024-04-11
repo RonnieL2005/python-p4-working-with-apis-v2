bd261f01a969bdf178c7b650d8aafbfa1915acff
Ronnie <ronnielangat2020@gmail.com>
2024-04-11 19:44:37 +0300

    m

--- lib/open_library_api.py
+++ lib/open_library_api.py
@@ -20,17 +20,17 @@ class Search:
         return response.content
 
     def get_search_results_json(self):
-        search_term = "the lord of the rings"
+     search_term = "the lord of the rings"
 
-        search_term_formatted = search_term.replace(" ", "+")
-        fields = ["title", "author_name"]
-        fields_formatted = ",".join(fields)
-        limit = 1
+     search_term_formatted = search_term.replace(" ", "+")
+     fields = ["title", "author_name"]
+     fields_formatted = ",".join(fields)
+     limit = 1
 
-        URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
-        print(URL)
-        response = requests.get(URL)
-        return response.json()
+     URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
+     print(URL)
+     response = requests.get(URL)
+     return response.json()
 
     def get_user_search_results(self, search_term):
         search_term_formatted = search_term.replace(" ", "+")
